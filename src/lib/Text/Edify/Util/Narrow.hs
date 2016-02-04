c41ce53cf5bc494b6ddb1bd2efde3c0396de4fab
Peter Jones <pjones@pmade.com>
2016-02-04 14:05:24 -0700

    Move language extensions to files, make narrowing more general

--- src/lib/Text/Edify/Util/Narrow.hs
+++ src/lib/Text/Edify/Util/Narrow.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 {-
 
 This file is part of the package edify. It is subject to the license
@@ -11,39 +13,58 @@ the LICENSE.md file.
 
 --------------------------------------------------------------------------------
 module Text.Edify.Util.Narrow
-       ( narrow
+       ( Markers (..)
+       , Token   (..)
+       , narrow
        , narrowToToken
        ) where
 
 --------------------------------------------------------------------------------
 -- Library imports.
 import Control.Monad
-import Data.Monoid
 import Data.Text (Text)
 import qualified Data.Text as T
+import Text.Parsec hiding (token)
+import Text.Parsec.Text
+
+--------------------------------------------------------------------------------
+-- | Beginning and ending markers for narrowing.
+data Markers = Markers Text Text
+
+--------------------------------------------------------------------------------
+-- | A bit of text that identifies a specific marker.
+data Token = Token Text
 
 --------------------------------------------------------------------------------
 -- | Narrow the given text using the default token delimiters.
-narrow :: Text -> Text -> Maybe Text
-narrow token = narrowToToken ("<<: " <> token <> "\n", ":>>\n")
+narrow :: Token -> Text -> Either String Text
+narrow = narrowToToken (Markers "<<:" ":>>")
 
 --------------------------------------------------------------------------------
 -- | Narrow the given text to a beginning and ending delimiter.
-narrowToToken :: (Text, Text) -> Text -> Maybe Text
-narrowToToken (start, end) = matchEnd <=< matchStart
-  where
-    matchStart :: Text -> Maybe Text
-    matchStart txt = case T.breakOn start txt of
-      (_, match) | T.null match -> Nothing
-                 | otherwise    -> Just $! stripStart match
+narrowToToken :: Markers -> Token -> Text -> Either String Text
+narrowToToken m t input =
+  case runParser (narrowP m t) () "" input of
+    Left e     -> Left (show e)
+    Right text -> Right text
 
-    matchEnd :: Text -> Maybe Text
-    matchEnd txt = case T.breakOn end txt of
-      (prefix, match) | T.null match -> Nothing
-                      | otherwise    -> Just $! stripEnd prefix
+--------------------------------------------------------------------------------
+-- | Extract the text between markers.
+narrowP :: Markers -> Token -> Parser Text
+narrowP (Markers start end) (Token token) = do
+  let beginning = string (T.unpack start) >> spaces >>
+                  string (T.unpack token) >> lookAhead space
 
-    stripStart :: Text -> Text
-    stripStart = T.drop (T.length start)
+  -- Skip over all characters until we hit the starting marker and the
+  -- token.  Then record what comes after the token.  This is useful
+  -- for languages like CSS that don't have single-line comments.
+  void (manyTill anyChar (try beginning) <?> "opening marker")
+  after <- manyTill anyChar endOfLine
 
-    stripEnd :: Text -> Text
-    stripEnd = T.dropWhileEnd (/= '\n')
+  let ending = string (T.unpack end) >> spaces >>
+               string (T.unpack . T.strip . T.pack $ after)
+
+  -- Now fetch all the characters *before* then ending marker.  Also
+  -- skip the entire line that the ending marker is on.
+  text <- manyTill anyChar (try ending) <?> "closing marker"
+  return (T.dropWhileEnd (/= '\n') . T.pack $ text)
