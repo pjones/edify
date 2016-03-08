bb7c730dfafdd595300c9c6913d381bbbb6e75e1
Peter Jones <pjones@pmade.com>
2016-03-07 17:55:12 -0700

    Update to LTS 5.5

--- build/nixpkgs.nix
+++ build/nixpkgs.nix
@@ -10,4 +10,10 @@ stdenv.mkDerivation {
     # Non-Haskell Dependencies:
     zlib
   ];
+
+    # Work around a bug in GHC:
+  # https://ghc.haskell.org/trac/ghc/ticket/11042
+  shellHook = ''
+    export LD_LIBRARY_PATH=${zlib}/lib
+  '';
 }
