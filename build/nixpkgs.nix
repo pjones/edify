b329c18c6053fd881f96b079d810c3341c5f5150
Peter Jones <pjones@pmade.com>
2016-06-08 15:19:11 -0700

    Ah, need both zlib and zlib.out now (Thanks NixOS)

--- build/nixpkgs.nix
+++ build/nixpkgs.nix
@@ -8,7 +8,8 @@ stdenv.mkDerivation {
     haskell.packages.lts-5_15.ghc
 
     # Non-Haskell Dependencies:
-    zlib.out
+    zlib      # For the header files.
+    zlib.out  # For the .so file.
   ];
 
   # Work around a bug in GHC:
