ca280bd37ee5aa3eb2d3c7125ecdcb4aa78d7184
Peter Jones <pjones@devalot.com>
2020-06-08 14:22:07 -0700

    Upgrade nix-hs

--- default.nix
+++ default.nix
@@ -1,26 +1,7 @@
-{ sources ? import ./nix/sources.nix
-, pkgs ? import sources.nixpkgs { }
-, nix-hs ? import sources.nix-hs { inherit pkgs; }
-, ghcide ? sources.ghcide-nix
-, ormolu ? sources.ormolu
-, ghc ? "default"
-}:
+{ sources ? import ./nix/sources.nix, pkgs ? import sources.nixpkgs { }
+, nix-hs ? import sources.nix-hs { inherit pkgs; }, ghc ? "default" }:
 
 nix-hs {
   cabal = ./edify.cabal;
   compiler = ghc;
-
-  overrides = lib: self: super: with lib; {
-    relude =
-      if super ? relude_0_6_0_0
-        then super.relude_0_6_0_0
-        else super.relude;
-
-    ghcide = import ghcide {};
-
-    ormolu = (import ormolu {
-      inherit (lib) pkgs;
-      ormoluCompiler = lib.compilerName;
-    }).ormolu;
-  };
 }
