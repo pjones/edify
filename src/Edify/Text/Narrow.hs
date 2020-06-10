5a17d2b3298031b97d1548d9d0b02007a8143933
Peter Jones <pjones@devalot.com>
2020-06-10 14:09:19 -0700

    Refactor text narrowing

---
+++ src/Edify/Text/Narrow.hs
@@ -0,0 +1,104 @@
+-- |
+--
+-- Copyright:
+--   This file is part of the package Edify. It is subject to the
+--   license terms in the LICENSE file found in the top-level
+--   directory of this distribution and at:
+--
+--     https://github.com/pjones/edify
+--
+--   No part of this package, including this file, may be copied,
+--   modified, propagated, or distributed except according to the terms
+--   contained in the LICENSE file.
+--
+-- License: Apache-2.0
+--
+-- Crop text based on starting and ending markers.
+module Edify.Text.Narrow
+  ( Markers (..),
+    Token (..),
+    NarrowError (..),
+    narrow,
+    narrowWith,
+    narrowP,
+  )
+where
+
+import qualified Data.Attoparsec.Text as Atto
+import Data.Char (isSpace)
+import qualified Data.Text as Text
+
+-- | Beginning and ending markers for narrowing.
+--
+-- @since 0.5.0.0
+data Markers = Markers Text Text
+  deriving (Show, Eq)
+
+-- | A bit of text that identifies a specific marker.
+--
+-- @since 0.5.0.0
+newtype Token = Token
+  {unToken :: Text}
+  deriving (Show, Eq)
+
+-- | Types of errors that may occur while narrowing.
+--
+-- @since 0.5.0.0
+newtype NarrowError = NarrowError String
+
+-- | Narrow the given text using the default token delimiters.
+--
+-- @since 0.5.0.0
+narrow :: Token -> Text -> Either NarrowError Text
+narrow = narrowWith (Markers "<<:" ":>>")
+
+-- | Narrow the given text to a beginning and ending delimiter.
+--
+-- @since 0.5.0.0
+narrowWith :: Markers -> Token -> Text -> Either NarrowError Text
+narrowWith m t input =
+  case Atto.parseOnly (narrowP m t) input of
+    Left e -> Left (NarrowError e)
+    Right text -> Right text
+
+-- | Parser that extracts the text between two markers that are
+-- identified with a name ('Token').   Only consumes enough input to
+-- extract the ending marker.
+--
+-- @since 0.5.0.0
+narrowP :: Markers -> Token -> Atto.Parser Text
+narrowP (Markers mstart mend) (Token token) = do
+  let beginning = do
+        _ <- Atto.string mstart
+        _ <- Atto.many1 Atto.space
+        _ <- Atto.string token
+        c <- Atto.peekChar'
+        if isSpace c
+          then -- Found the opening marker and token.
+            pure ()
+          else -- Found token with same prefix, backtrack.
+            empty
+      ending = do
+        _ <- Atto.string mend
+        c <- Atto.peekChar' <|> pure '\n'
+        if isSpace c
+          then -- Found the closing marker.
+            pure ()
+          else -- Looks like the closing marker, except that the
+          -- character after the marker isn't a space or EOF.
+            empty
+
+  -- Skip over all characters until we hit the starting marker and the
+  -- token.
+  _ <-
+    Atto.manyTill Atto.anyChar beginning
+      Atto.<?> ("opening marker " <> toString mstart)
+
+  -- Skill all other characters on the current line.
+  _ <- Atto.manyTill Atto.anyChar Atto.endOfLine
+
+  -- Now fetch all the characters *before* the ending marker.  Also
+  -- skip the entire line that the ending marker is on.
+  Atto.manyTill Atto.anyChar ending
+    <&> (toText >>> Text.dropWhileEnd (/= '\n'))
+    Atto.<?> ("closing marker " <> toString mend)
